3dd93190106aadd58194f8c3358b541255b10cee
Alex Levenson <alex@isnotinvain.com>
2025-12-26 02:04:29 -0500

    update omni84 for all qualities

--- src/voicing_styles/Omni84.h
+++ src/voicing_styles/Omni84.h
@@ -8,17 +8,24 @@
 
 class Omni84 : public VoicingStyle<VoicingFor::Chord> {
    public:
+    // clang-format off
     static inline const std::map<ChordQuality, int> OCTAVE_BEGIN_MAP = {
-        {ChordQuality::MAJOR, 36},
-        {ChordQuality::MINOR, 48},
-        {ChordQuality::DOM_7, 60},
+        {ChordQuality::MAJOR,     36},
+        {ChordQuality::MINOR,     48},
+        {ChordQuality::DOM_7,     60},
+        {ChordQuality::MINOR_7,   72},
+        {ChordQuality::MAJOR_7,   84},
+        {ChordQuality::DIM_7,     96},
+        {ChordQuality::AUGMENTED, 108},
     };
+    // clang-format on
 
     Omni84() = default;
 
     std::string displayName() const override { return "Omni-84"; }
     std::string description() const override {
-        return "Outputs only a single root note, octave shifted to match what Omni-84 expects for the current chord quality.";
+        return "Outputs only a single root note, octave shifted to match what Omni-84 expects for the current chord quality.\n\nNote: Doesn't "
+               "support Sus4 or Add9.";
     }
 
     std::vector<int> constructChord(ChordQuality quality, int root) const override {
